36664b9a942a70a5944c44350a15aaa88a58cdb4
补档PO <25119890+BDPO@users.noreply.github.com>
2019-07-05 14:06:37 +0800

    acfun.cn/a/ac 文章页跳转 z/ac

---
+++ aac2zac.user.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name        AcFun自动换地址 V3文章版
+// @description 目前可以调用旧版评论区风格，不保证后续可用
+
+// @author      补档PO
+// @version     1.3.2
+
+// @include     https://www.acfun.cn/a/ac*
+
+// ==/UserScript==
+
+window.location.href = window.location.href.replace(/^https:\/\/www.\acfun\.cn\/a\//, 'https:\/\/www.\acfun\.cn\/z\/');
