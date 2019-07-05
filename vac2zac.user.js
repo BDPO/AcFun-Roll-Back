fcfdb9299e0764254624887268ea5938ad79a63f
补档PO <25119890+BDPO@users.noreply.github.com>
2019-07-05 14:18:33 +0800

    acfun.cn/v/ac 视频页跳转 z/ac

--- vac2zac.user.js
+++ vac2zac.user.js
@@ -1,9 +1,24 @@
 // ==UserScript==
-// @name        AcFun自动换地址 V3视频版
-// @description 视频页面调用flash播放器【已失效】
-// @description 目前可以调用旧版评论区风格，不保证后续可用
-// @author      补档PO
-// @version     1.3.1
-// @include     https://www.acfun.cn/v/ac*
+// @name        AcFun自动换地址 视频版
+
+// @description 视频页面调用flash播放器
+// @description 旧版评论区风格，不保证后续可用
+
+// @description 感谢 浮云总是梦 全程指导代码！
+// @author      浮云总是梦 补档PO
+// @version     1.3.3
+
+// @include     https://www.acfun.cn/*
+
 // ==/UserScript==
-window.location.href = window.location.href.replace(/^https:\/\/www.\acfun\.cn\/v\//, 'https:\/\/www.\acfun\.cn\/z\/');
+
+/* 使用 if 语句 判断当前网页URL是否在 v/ac 视频详情页
+=true 执行 window.location.href.replace 语句 转z/ac
+=false 脚本无动作
+*/
+if (window.location.href.indexOf('www.acfun.cn/v/ac') > 0) {
+  window.location.href = window.location.href.replace('www.acfun.cn/v', 'www.acfun.cn/z')
+}
+
+// 使用document.getElementById()语句，给"ACFlashPlayer-re"元素下的"data"内容赋予httpsURL，使得flash播放器正常工作
+document.getElementById("ACFlashPlayer-re").data = "https://cdn.aixifan.com/flash/player-view-homura.swf"
